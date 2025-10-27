9054c34138ccfceb3db586812b9a0144133ac374
Jaidil14 <jaidilrachmad@gmail.com>
2025-10-27 10:01:56 +0700

    menambahkan method CRUD

--- index.js
+++ index.js
@@ -20,3 +20,58 @@ db.sequelize.sync()
     .catch((err) => {
         console.log(err);
     })
+
+app.post("/komik", async (req, res) => {
+    const data = req.body;
+    try {
+        const komik = await db.Komik.create(data); 
+        res.status(201).send({
+            message: "Komik berhasil ditambahkan!",
+            komik: komik
+        });
+    } catch (err) {
+        res.status(500).send
+    }
+});
+
+app.get('/komik', async (req,res) => {
+    try {
+        const komik = await db.komik.findAll();
+        res.send(komik);
+    }
+    catch (err) {
+        res.send(err);
+    }
+});
+
+app.put('/komik/:id', async (req,res) => {
+    const id = req.params.id;
+    const data = req.body;
+
+    try {
+        const komik = await db.Komik.findByPk(id);
+        if (!komik) {
+            return res.status(404).send({ message: 'Komik tidak ditemukan' });
+        }
+
+        await komik.update(data);
+        res.send({ message: 'Komik berhasil diupdate', komik });
+    }catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+app.delete('/komik/:id', async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const komik = await db.Komik.findByPk(id);
+        if (!komik) {
+            return res.status(404).send({ message: 'Komik tidak ditemukan untuk dihapus' });
+        }
+        await komik.destroy();
+        res.send({ message: 'Komik berhasil dihapus' });
+    } catch (err) {
+        res.status(500).send
+    }
+});
